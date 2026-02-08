099b7ba952d0086d320ad1d2fa4544090c1f7ef6
Vladimir Kulgun <koulgoun@yandex.ru>
2026-02-08 22:31:02 +0200

    task6

--- Task6/00_minikube_up.bat
+++ Task6/00_minikube_up.bat
@@ -4,12 +4,7 @@ mkdir "%USERPROFILE%\.minikube\files\etc\ssl\certs" 2>nul
 copy /Y ".\audit-policy.yaml" "%USERPROFILE%\.minikube\files\etc\ssl\certs\audit-policy.yaml"
 
 docker desktop start
-minikube start --driver=docker \
-  --extra-config=apiserver.audit-log-path=- \
-  --extra-config=apiserver.audit-policy-file=/etc/ssl/certs/audit-policy.yaml
 
-minikube addons enable metrics-server
-minikube addons enable default-storageclass
-minikube addons enable storage-provisioner
+minikube start --driver=docker --cpus=4 --extra-config=apiserver.audit-log-path=- --extra-config=apiserver.audit-policy-file=/etc/ssl/certs/audit-policy.yaml
 
 minikube status
